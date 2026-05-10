4616cdfd77c3bf5ebb98dbf00206af4133bb8883
Salsabila Fitria K. <salsabila.khair@mail.kmutt.ac.th>
2026-05-10 21:58:35 +0700

    Update time_travel.h

--- time_travel.h
+++ time_travel.h
@@ -1,24 +1,9 @@
 #ifndef TIME_TRAVEL_H
 #define TIME_TRAVEL_H
 
-#define MAX_HISTORY 5
-#define MAX_CHILDREN 10
-
-typedef struct Folder {
-    char name[50];
-    struct Folder* children[MAX_CHILDREN];
-    int child_count;
-} Folder;
-
-typedef struct {
-    int hp;
-    char current_location[50];
-    Folder* folder_snapshot;
-} GameState;
-
-Folder* create_folder(char* name);
-Folder* copy_tree(Folder* root);
-void save_checkpoint(int hp, char* loc, Folder* root);
-void time_travel(int version, int* current_hp, char* current_loc, Folder** current_root);
+void saveGame(int slot, int wave, int pHP, int vHP);
+int loadGame(int slot, int *wave, int *pHP, int *vHP);
+int execute_save_sequence(int slot, int wave, int pHP, int vHP);
+int execute_load_sequence(int slot, int *wave, int *pHP, int *vHP);
 
 #endif
