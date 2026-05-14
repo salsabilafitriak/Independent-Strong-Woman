b12cea6e7a1098e6ae50b718af63ff9a321bccbb
Salsabila Fitria K. <salsabila.khair@mail.kmutt.ac.th>
2026-05-14 15:21:04 +0700

    Update history.h

--- history.h
+++ history.h
@@ -1,15 +1,17 @@
 #ifndef HISTORY_H
 #define HISTORY_H
 
-#include "fileSystem.h" 
+#include "fileSystem.h"
 
 typedef struct StackNode {
-    Folder *folder_ptr;        
-    struct StackNode *next;    
+    Folder         *folder_ptr;  
+    struct StackNode *next;      
 } StackNode;
 
-void push_history(StackNode **top, Folder *f);
+void    push_history(StackNode **top, Folder *f);
 Folder* pop_history(StackNode **top);
-void clear_history(StackNode **top);
+Folder* peek_history(StackNode *top);
+void    clear_history(StackNode **top);
+int     stack_depth(StackNode *top);
 
 #endif
