6b28e76f172e72cf94c2391508caaa848f3da6ce
Salsabila Fitria K. <salsabila.khair@mail.kmutt.ac.th>
2026-05-14 15:23:22 +0700

    Update time_travel.h

--- time_travel.h
+++ time_travel.h
@@ -1,9 +1,13 @@
 #ifndef TIME_TRAVEL_H
 #define TIME_TRAVEL_H
 
+
+#define MAX_SLOTS 3
+
 void saveGame(int slot, int wave, int pHP, int vHP);
-int loadGame(int slot, int *wave, int *pHP, int *vHP);
-int execute_save_sequence(int slot, int wave, int pHP, int vHP);
-int execute_load_sequence(int slot, int *wave, int *pHP, int *vHP);
+int  loadGame(int slot, int *wave, int *pHP, int *vHP);
+int  execute_save_sequence(int slot, int wave, int pHP, int vHP);
+int  execute_load_sequence(int slot, int *wave, int *pHP, int *vHP);
+void reset_slot_usage(void);
 
 #endif
