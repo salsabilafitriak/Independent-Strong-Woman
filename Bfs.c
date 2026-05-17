06a1a33cdf0467fade7d2ecf9070e3e81172cb2a
Salsabila Fitria K. <salsabila.khair@mail.kmutt.ac.th>
2026-05-17 08:45:20 +0700

    Update Bfs.c

--- Bfs.c
+++ Bfs.c
@@ -28,43 +28,16 @@ Folder* bfs_dequeue(BFSQueue *q) {
     return f;
 }
 
-Folder* bfs_spread_virus(Folder *root, int wave) {
-    Folder *infected[BFS_QUEUE_SIZE];
-    int inf_count = 0;
+Folder* bfs_find_virus(Folder *root) {
+    BFSQueue q;
+    bfs_queue_init(&q);
+    bfs_enqueue(&q, root);
 
-    BFSQueue scan_q;
-    bfs_queue_init(&scan_q);
-    bfs_enqueue(&scan_q, root);
-    while (!bfs_queue_empty(&scan_q)) {
-        Folder *cur = bfs_dequeue(&scan_q);
-        if (cur->has_virus && inf_count < BFS_QUEUE_SIZE)
-            infected[inf_count++] = cur;
+    while (!bfs_queue_empty(&q)) {
+        Folder *cur = bfs_dequeue(&q);
+        if (cur->has_virus) return cur;
         for (int i = 0; i < cur->child_count; i++)
-            bfs_enqueue(&scan_q, cur->children[i]);
+            bfs_enqueue(&q, cur->children[i]);
     }
-
-    if (inf_count == 0) return NULL;
-
-    int spread_limit = (wave == 1) ? 1 : (wave == 2) ? 2 : MAX_CHILDREN;
-    Folder *last_spread = NULL;
-
-    for (int i = 0; i < inf_count; i++) {
-        Folder *src = infected[i];
-        int spread_count = 0;
-
-        for (int j = 0; j < src->child_count && spread_count < spread_limit; j++) {
-            Folder *neighbor = src->children[j];
-            if (!neighbor->has_virus) {
-                neighbor->infection_level++;
-                if (neighbor->infection_level >= 2) {
-                    neighbor->has_virus = 1; 
-                    last_spread = neighbor;
-                }
-                spread_count++;
-            }
-        }
+    return NULL;
 }
-    return last_spread;
-}
-
-
